43e808ac9ade40e905229dc151927c6500c83dfa
LieutenantCrunch <52416950+LieutenantCrunch@users.noreply.github.com>
2022-03-04 14:44:56 -0500

    Update SpecialSection.jsx
    Added comment about special spells not being supported

--- src/js/components/Spell/SpecialSection.jsx
+++ src/js/components/Spell/SpecialSection.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import Typography from '@mui/material/Typography';
+
 export const SpecialSection = ({ }) => {
     return (
         <div>
-
+            <Typography variant="subtitle1">
+                Due to the nature of Special Spells, this page does not support them
+            </Typography>
         </div>
     );
 };
