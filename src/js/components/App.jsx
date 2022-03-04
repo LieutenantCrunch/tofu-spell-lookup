3b905123c374bee4da5a31baee9a99db9ad65502
LieutenantCrunch <52416950+LieutenantCrunch@users.noreply.github.com>
2022-03-04 14:02:46 -0500

    Generic Warning
    Added a generic warning to the page so users would know the appearance is not 100% accurate

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -3,13 +3,23 @@ import React from 'react';
 // MUI
 import Container from '@mui/material/Container';
 import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 import { FrameContainer } from './Frame/FrameContainer';
 import { SpellContainer } from './Spell/SpellContainer';
 
 export const App = ({ }) => {
     return <>
-        <Toolbar />
+        <Toolbar
+            style={{
+                display: 'flex',
+                justifyContent: 'center'
+            }}
+        >
+            <Typography variant="subtitle1">
+                Please Note: This only provides an approximation of what your card will look like and is not 100% accurate.
+            </Typography>
+        </Toolbar>
         <Container>
             <div
                 style={{
