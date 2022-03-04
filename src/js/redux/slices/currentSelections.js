51aa8c464b869ad79b1c26f13467f6ca213915f8
LieutenantCrunch <52416950+LieutenantCrunch@users.noreply.github.com>
2022-03-04 14:10:37 -0500

    Font Support
    Added support for Fonts Spells

--- src/js/redux/slices/currentSelections.js
+++ src/js/redux/slices/currentSelections.js
@@ -7,9 +7,13 @@ const initialState = {
     continuousRotate: undefined,
     frame: {
         "name": "Default",
-        "image": "default"
+        "image": "default",
+        "defaultFont": "Gligoth",
+        "defaultColor": "hsl(0,0%,0%)"
     },
     font: undefined,
+    name: 'Name',
+    series: 'Series',
     special: undefined,
     specificRotate: undefined,
     textColor: undefined
@@ -19,6 +23,9 @@ const currentSelectionsSlice = createSlice({
     name: 'currentSelections',
     initialState,
     reducers: {
+        setCurrentFont: (state, action) => {
+            state.font = action.payload;
+        },
         setCurrentFrame: (state, action) => {
             state.frame = action.payload;
         },
@@ -26,6 +33,9 @@ const currentSelectionsSlice = createSlice({
             state.hue = action.payload;
             state.continuousRotate = undefined
             state.specificRotate = undefined;
+        },
+        setCurrentName: (state, action) => {
+            state.name = action.payload;
         },
         setContinuousRotate: (state, action) => {
             state.hue = undefined;
@@ -36,6 +46,9 @@ const currentSelectionsSlice = createSlice({
             state.hue = undefined;
             state.continuousRotate = action.payload;
             state.specificRotate = action.payload;
+        },
+        setCurrentSeries: (state, action) => {
+            state.series = action.payload;
         }
     }
 });
@@ -43,14 +56,20 @@ const currentSelectionsSlice = createSlice({
 export default currentSelectionsSlice.reducer;
 
 export const {
+    setCurrentFont,
     setCurrentFrame,
     setCurrentHue,
+    setCurrentName,
     setContinuousRotate,
-    setSpecificRotate
+    setSpecificRotate,
+    setCurrentSeries
 } = currentSelectionsSlice.actions;
 
+export const selectCurrentFont = state => state.currentSelections.font;
 export const selectCurrentFrame = state => state.currentSelections.frame;
 export const selectCurrentHue = state => state.currentSelections.hue;
+export const selectCurrentName = state => state.currentSelections.name;
+export const selectCurrentSeries = state => state.currentSelections.series;
 export const selectContinuousRotate = state => state.currentSelections.continuousRotate;
 export const selectSpecificRotate = state => state.currentSelections.specificRotate;
 
