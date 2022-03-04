117532a4fdec40db040219e5c2ceb38e8a8ca95f
LieutenantCrunch <52416950+LieutenantCrunch@users.noreply.github.com>
2022-03-04 14:08:27 -0500

    Create ImageSection.jsx
    Broke the image into its own component for performance and so the text section could be positioned within the same parent component

---
+++ src/js/components/Frame/ImageSection.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+// Redux
+import { useSelector } from 'react-redux';
+import {
+    selectCurrentFrame,
+    selectContinuousRotate,
+    selectCurrentHue
+} from '../../redux/slices/currentSelections';
+
+export const ImageSection = ({ }) => {
+    const currentFrame = useSelector(selectCurrentFrame);
+    const continuousRotate = useSelector(selectContinuousRotate);
+    const currentHue = useSelector(selectCurrentHue);
+
+    let parentBackgroundImage = currentFrame && currentHue ? `url('i/${currentFrame.image}.hue.png')` : 'none';
+    let parentFilter = currentHue ? `hue-rotate(${currentHue.value}deg)` : 'none';
+    let imageFilter = continuousRotate 
+        ? `hue-rotate(${continuousRotate.value}deg)` 
+        : (
+            currentHue
+            ? `hue-rotate(-${currentHue.value}deg)`
+            : 'none'
+        );
+
+    return (
+        <div
+            style={{
+                backgroundImage: parentBackgroundImage,
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'contain',
+                filter: parentFilter,
+                height: '100%',
+                width: '100%'
+            }}
+        >
+            {
+                currentFrame &&
+                <img
+                    src={`i/${currentFrame.image}.png`}
+                    style={{
+                        filter: imageFilter,
+                        height: '100%',
+                        mixBlendMode: 'luminosity',
+                        width: '100%'
+                    }}
+                />
+            }
+        </div>
+    );
+};
