ca620b0d08f61a2871715252ea6438b9b02ba57c
LieutenantCrunch <52416950+LieutenantCrunch@users.noreply.github.com>
2022-03-07 11:58:44 -0500

    Tofu link
    Added note about Tofu owning the frame images

--- src/js/components/Frame/FrameContainer.jsx
+++ src/js/components/Frame/FrameContainer.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+// MUI
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+
+// Other Components
 import { FramePreview } from './FramePreview';
 import { FrameSelect } from './FrameSelect';
 
@@ -15,6 +20,15 @@ export const FrameContainer = ({ }) => {
         >
             <FrameSelect />
             <FramePreview />
+            <Typography
+                sx={{
+                    maxWidth: '300px',
+                    wordWrap: 'wrap'
+                }}
+                variant="subtitle2"
+            >
+                All frame images are property of <Link href="https://top.gg/bot/792827809797898240">Tofu</Link>, images shown here are for demonstration purposes only.
+            </Typography>
         </div>
     );
 };
